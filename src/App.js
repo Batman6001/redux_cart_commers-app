e56b26c68f1e66c1c74d022e230c64510d65be29
sushil sing <sushilsingh11598@gmail.com>
2023-07-18 21:59:16 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
-import "./App.css"
+// import "./App.css"
+import Footer from './components/Footers/Footer'
+import Footers from './components/Footers/Footers'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ShopAll from './components/ShopAll/ShopAll';
+
 
 function App() {
   return (
-    <div className='App'>App new</div>
+    <div className='App'>
+      <ShopAll />
+      <Footer />
+      {/* <Footers /> */}
+    </div>
   )
 }
 
