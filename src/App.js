665a65cc60432ec968294748ac125a977d22b1df
sushil sing <sushilsingh11598@gmail.com>
2023-07-19 18:07:46 +0530

    about

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
 // import "./App.css"
 import Footer from './components/Footers/Footer'
-import Footers from './components/Footers/Footers'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ShopAll from './components/ShopAll/ShopAll';
+// import Footers from './components/Footers/Footers'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Whyflatheads from './components/Abouts/Whyflatheads';
+// import OurStory from './components/Abouts/OurStory';
+import CustomarReviews from './components/Abouts/CustomarReviews';
 
 
 function App() {
   return (
     <div className='App'>
-      <ShopAll />
+    <CustomarReviews />
+    {/* <OurStory /> */}
+      {/* <Whyflatheads /> */}
       <Footer />
       {/* <Footers /> */}
     </div>
---
+++ src/components/Abouts/CustomarReviews.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './indexabout.css'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+const CustomarReviews = () => {
+    return (
+        <div className="error-page">
+            <h1 className="error-page__heading">
+                Page Not Found
+            </h1>
+            <p>
+                We&#39;re sorry, but the page you requested does not exist.
+            </p>
+            <p>
+                Try searching or <a href="all" className='a_page'>continue shopping</a>
+            </p>
+        </div>
+    )
+}
+
+export default CustomarReviews
