168cb0e9ae91c9bdc27dcadbeb18da549b3aab91
sushil sing <sushilsingh11598@gmail.com>
2023-07-20 14:14:46 +0530

    about

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-// import Footer from './components/Footers/Footer'
+
 // import Footers from './components/Footers/Footers'
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import Whyflatheads from './components/Abouts/Whyflatheads';
+// import Whyflatheads from './components/Abouts/Whyflatheads';
 // import OurStory from './components/Abouts/OurStory';
 // import CustomarReviews from './components/Abouts/CustomarReviews';
 import Blog from './components/Abouts/Blog';
 // import InThePress from './components/Abouts/InThePress';
 
-// import Header from './Screen/Header/index';
+import Header from './Screen/Header/index';
 
 
 // import HomePage from './Screen/Home/index' 
@@ -17,13 +17,13 @@ import Blog from './components/Abouts/Blog';
 function App() {
   return (
     <div className='App'>
-      {/* <Header /> */}
+      <Header />
       {/* <InThePress /> */}
       <Blog />
       {/* <CustomarReviews /> */}
       {/* <OurStory /> */}
       {/* <Whyflatheads /> */}
-      {/* <Footer /> */}
+      {/* <Foo */}
       {/* <Footers /> */}
       {/* <HomePage/> */}
 
--- src/components/Abouts/Blog.js
+++ src/components/Abouts/Blog.js
@@ -1,48 +1,107 @@
 import React from 'react'
 import './indexabout.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Blog = () => {
     return (
         <>
-            <div class="breadcrumbs">
-                <a href="/" class="breadcrumbs__link">Home</a>
+            <div className="main-container-blog">
+                <div class="breadcrumbs">
+                    <a href="/" class="breadcrumbs__link">Home</a>
 
-                <span class="breadcrumbs__current">Flatheads Blog</span>
+                    <span class="breadcrumbs__current">Flatheads Blog</span>
+                </div>
+                <div class="blog-template__content">
+
+                    <h1 class="blog-template__heading">
+                        Flatheads Blog
+                    </h1>
+                </div>
+
+                <div class="cart-container">
+                    <div class="cart-item">
+                        <img src="//www.flatheads.in/cdn/shop/articles/Softknit_banner_1024x417.jpg?v=1649170770" alt="5" />
+                        <p className='cart-item-content1'> 5 reasons why you should try these super comfortable loafers</p>
+                        <p className='cart-item-content2'>April 05, 2022</p>
+                        <p className='cart-item-content3'>Perhaps the most versatile footwear option available to men and women, the loafer is simply an addition you should make to your footwear collection. And we couldn’t think of writing this</p>
+                        <u> <h6>CONTINUE READING</h6></u>
+                    </div>
+                    <div class="cart-item">
+                        <img src="//www.flatheads.in/cdn/shop/articles/Bamboo_banner_1024x417.jpg?v=1648621963" alt="Here" />
+                        <p className='cart-item-content1'>Here’s why you MUST add bamboo shoes to your wardrobe!</p>
+                        <p className='cart-item-content2'>March 30, 2022</p>
+                        <p className='cart-item-content3'>As time rolls on and the world around us changes, it becomes important to change with it. With increasingly conscious consumers, come conscious brands making products that not only</p>
+                        <u> <h6>CONTINUE READING</h6></u>
+                    </div>
+                    <div class="cart-item">
+                        <img src="//www.flatheads.in/cdn/shop/articles/Linen_Hero_banner_1024x417.jpg?v=1646905909" alt="Here" />
+                        <p className='cart-item-content1'>A first look at India’s first Linen Sneakers</p>
+                        <p className='cart-item-content2'>March 10, 2022</p>
+                        <p className='cart-item-content3'>Did you know that there was a period in time when linen clothing was only available to the creme de la creme of society? It was a luxury fabric that not everyone was fortunate enough to</p>
+                        <u> <h6>CONTINUE READING</h6></u>
+                    </div>
+                    {/* ________2_______________ */}
+
+                    <div class="cart-item">
+                        <img src="//www.flatheads.in/cdn/shop/articles/Kooltex_blog_banner_1024x417.jpg?v=1646314778" alt="5" />
+                        <p className='cart-item-content1'> What’s cool about KoolTex? An explainer on Dry Fit trainers.</p>
+                        <p className='cart-item-content2'>March 03, 2022</p>
+                        <p className='cart-item-content3'>Perhaps the most important criteria when selecting footwear is how good it makes you feel. When you’re on your feet all day long, it only makes sense to invest in footwear that helps you...</p>
+                        <u> <h6>CONTINUE READING</h6></u>
+                    </div>
+                    <div class="cart-item">
+                        <img src="//www.flatheads.in/cdn/shop/articles/Bana_1024x417.jpg?v=1645967955" alt="Here" />
+                        <p className='cart-item-content1'>Bet you’ve never seen a shoe like this: Meet the world’s first Banana Kicks.</p>
+                        <p className='cart-item-content2'>February 27, 2022</p>
+                        <p className='cart-item-content3'>What’s the deal, banana peel?
+                            Not sure about peels but banana stems sure are a big deal for us. Read more to find out why.
+                            ...</p>
+                        <u> <h6>CONTINUE READING</h6></u>
+                    </div>
+                    <div class="cart-item">
+                        <img src="//www.flatheads.in/cdn/shop/articles/Blog_banner2_1024x417.jpg?v=1645781853" alt="Here" />
+                        <p className='cart-item-content1'>Launching not one but four, ‘first-of-its-kind’ collections!</p>
+                        <p className='cart-item-content2'>February 24, 2022</p>
+                        <p className='cart-item-content3'>Those who have followed our journey already know how obsessed we are with breaking the rules. So it should come as no surprise that we did it again. We identified new materials and...</p>
+                        <u> <h6>CONTINUE READING</h6></u>
+                    </div>
+
+                    {/* ___________3____________ */}
+
+                    <div class="cart-item">
+                        <img src="//www.flatheads.in/cdn/shop/articles/Cover_Image_We_re_on_a_mission_1024x768.jpg?v=1644212916" alt="5" />
+                        <p className='cart-item-content1'> What’s cool about KoolTex? An explainer on Dry Fit trainers.</p>
+                        <p className='cart-item-content2'>March 03, 2022</p>
+                        <p className='cart-item-content3'>Perhaps the most important criteria when selecting footwear is how good it makes you feel. When you’re on your feet all day long, it only makes sense to invest in footwear that helps you...</p>
+                        <u> <h6>CONTINUE READING</h6></u>
+                    </div>
+                    <div class="cart-item">
+                        <img  src="//www.flatheads.in/cdn/shop/articles/Blog3_1024x320.jpg?v=1629448534" alt="Here" />
+                        <p className='cart-item-content1'>Bet you’ve never seen a shoe like this: Meet the world’s first Banana Kicks.</p>
+                        <p className='cart-item-content2'>February 27, 2022</p>
+                        <p className='cart-item-content3'>What’s the deal, banana peel?
+                            Not sure about peels but banana stems sure are a big deal for us. Read more to find out why.
+                            ...</p>
+                        <u> <h6>CONTINUE READING</h6></u>
+                    </div>
+                    
+                    <div class="cart-item">
+                        <img src="//www.flatheads.in/cdn/shop/articles/blog_2_creative-01-optimised_1024x768.jpg?v=1624590245" alt="Here" />
+                        <p className='cart-item-content1'>Launching not one but four, ‘first-of-its-kind’ collections!</p>
+                        <p className='cart-item-content2'>February 24, 2022</p>
+                        <p className='cart-item-content3'>Those who have followed our journey already know how obsessed we are with breaking the rules. So it should come as no surprise that we did it again. We identified new materials and...</p>
+                        <u> <h6>CONTINUE READING</h6></u>
+                    </div>
+                    <div class="cart-item">
+                        <img src="//www.flatheads.in/cdn/shop/articles/blog_1-01_-_1200_900-optimised_1024x768.jpg?v=1622684422" alt="Here" />
+                        <p className='cart-item-content1'>Launching not one but four, ‘first-of-its-kind’ collections!</p>
+                        <p className='cart-item-content2'>February 24, 2022</p>
+                        <p className='cart-item-content3'>Those who have followed our journey already know how obsessed we are with breaking the rules. So it should come as no surprise that we did it again. We identified new materials and...</p>
+                        <u> <h6>CONTINUE READING</h6></u>
+                    </div>
+                </div >
             </div>
-            <div class="blog-template__content">
 
-                <h1 class="blog-template__heading">
-                    Flatheads Blog
-                </h1>
-            </div>
-
-            <div class="cart-container">
-                <div class="cart-item">
-                    <img src="//www.flatheads.in/cdn/shop/articles/Softknit_banner_1024x417.jpg?v=1649170770" alt="5" />
-                    <p className='cart-item-content1'> 5 reasons why you should try these super comfortable loafers</p>
-                    <p className='cart-item-content2'>April 05, 2022</p>
-                    <p className='cart-item-content3'>Perhaps the most versatile footwear option available to men and women, the loafer is simply an addition you should make to your footwear collection. And we couldn’t think of writing this</p>
-                    <u> <h6>CONTINUE READING</h6></u>
-                </div>
-                <div class="cart-item">
-                    <img src="//www.flatheads.in/cdn/shop/articles/Bamboo_banner_1024x417.jpg?v=1648621963" alt="Here" />
-                    <p className='cart-item-content1'>Here’s why you MUST add bamboo shoes to your wardrobe!</p>
-                    <p className='cart-item-content2'>March 30, 2022</p>
-                    <p className='cart-item-content3'>As time rolls on and the world around us changes, it becomes important to change with it. With increasingly conscious consumers, come conscious brands making products that not only</p>
-                    <u> <h6>CONTINUE READING</h6></u>
-                </div>
-                <div class="cart-item">
-                    <img src="//www.flatheads.in/cdn/shop/articles/Linen_Hero_banner_1024x417.jpg?v=1646905909" alt="Here" />
-                    <p className='cart-item-content1'>A first look at India’s first Linen Sneakers</p>
-                    <p className='cart-item-content2'>March 10, 2022</p>
-                    <p className='cart-item-content3'>Did you know that there was a period in time when linen clothing was only available to the creme de la creme of society? It was a luxury fabric that not everyone was fortunate enough to</p>
-                    <u> <h6>CONTINUE READING</h6></u>
-                </div>
-                
-            </div >
-
-            
 
             {/* <div className="blog_main">
                 <p>Home > <span>Flatheads Blog</span></p>
